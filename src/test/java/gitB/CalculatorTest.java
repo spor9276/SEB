3ea1ddf3f54f12e393587cf60ba06e0962768536
ischung <insang@hansung.ac.kr>
2016-04-27 14:41:34 +0900

    add기능추가

---
+++ src/test/java/gitB/CalculatorTest.java
@@ -0,0 +1,16 @@
+package gitB;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAdd() {
+		Calculator cal = new Calculator();
+		cal.add(10,  20);
+		assertEquals(30, cal.getResult());
+	}
+
+}
